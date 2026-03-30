7e1a7bc95bf95dbb5340815a95c3097751ee99c9
pptemaa <pptemaa@gmail.com>
2026-03-30 16:04:50 +0300

    начальная настройка проекта Spring Boot

---
+++ src/main/java/com/example/scooterrentalsystem/ScooterRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.scooterrentalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScooterRentalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScooterRentalSystemApplication.class, args);
+    }
+
+}
